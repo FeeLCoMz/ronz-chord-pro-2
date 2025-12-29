543134df2017df93789e14e946ae202927bca982
FeeLCoMz <novicalz@gmail.com>
2025-12-30 03:27:52 +0700

    feat: update server port configuration and add proxy settings for API

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     open: true,
-    host: true
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   },
   build: {
     outDir: 'dist',
